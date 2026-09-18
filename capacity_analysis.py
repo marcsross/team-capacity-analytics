9818827c99ada29ac01b62f154c1f9e6ae5e8074
Marcos Adan Rosas Medina <rosas250400@gmail.com>
2026-09-18 14:08:18 +0000

    Add sample team capacity data

--- capacity_analysis.py
+++ capacity_analysis.py
@@ -1,1 +1,12 @@
-print("Team Capacity Analytics")
+team_members = ["Alex", "Jordan", "Taylor", "Morgan"]
+
+active_projects = [7, 4, 9, 5]
+
+print("Team members:")
+print(team_members)
+
+print("Active projects:")
+print(active_projects)
+
+print("Number of team members")
+print(len(team_members))
